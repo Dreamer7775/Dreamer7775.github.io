adba222b8c921abaa8dd1bd2b0a66a6e20273874
Dreamer7775 <109203455+Dreamer7775@users.noreply.github.com>
2022-07-14 14:15:06 +0200

    Add files via upload

--- Script.js
+++ Script.js
@@ -2,9 +2,134 @@ function login(){
 	var user =document.getElementById('User').value;
 	var pass =document.getElementById('pass').value;
 	if (user=="777" && pass=="5") {
-		var elem = document.createElement("img");
-		elem.setAttribute("src", "https://www4.minijuegosgratis.com/v3/games/thumbnails/240546_1.jpg");
+		var elem = new Image(400, 300); // width, height
+		elem.src = "https://www4.minijuegosgratis.com/v3/games/thumbnails/240546_1.jpg";
 		document.getElementById("placehere").appendChild(elem);
+
 		
 	}
 }	
+
+
+function vegetta() {
+	document.getElementById('discord').play();
+	setTimeout(function(){
+		window.location.href='https://www.youtube.com/results?search_query=vegetta777';
+	}, 500);
+
+}
+
+function Willyrex() {
+	document.getElementById('discord').play();
+	setTimeout(function(){
+		window.location.href='https://www.youtube.com/results?search_query=Willyrex';
+	}, 500);
+
+}
+
+function Rubius() {
+	document.getElementById('discord').play();
+	setTimeout(function(){
+		window.location.href='https://www.youtube.com/results?search_query=Rubius';
+	}, 500);
+
+}
+
+function Fargan() {
+	document.getElementById('discord').play();
+	setTimeout(function(){
+		window.location.href='https://www.youtube.com/results?search_query=Fargan';
+	}, 500);
+
+}
+
+function Luzu() {
+	document.getElementById('discord').play();
+	setTimeout(function(){
+		window.location.href='https://www.youtube.com/results?search_query=Luzu';
+	}, 500);
+
+}
+
+function Alexby11() {
+	document.getElementById('discord').play();
+	setTimeout(function(){
+		window.location.href='https://www.youtube.com/results?search_query=Alexby11';
+	}, 500);
+
+}
+
+function Lolito() {
+	document.getElementById('discord').play();
+	setTimeout(function(){
+		window.location.href='https://www.youtube.com/results?search_query=Lolito';
+	}, 500);
+
+}
+
+function IlloJuan() {
+	document.getElementById('discord').play();
+	setTimeout(function(){
+		window.location.href='https://www.youtube.com/results?search_query=IlloJuan';
+	}, 500);
+
+}
+
+function Staxx() {
+	document.getElementById('discord').play();
+	setTimeout(function(){
+		window.location.href='https://www.youtube.com/results?search_query=Staxx';
+	}, 500);
+
+}
+
+function Mangel() {
+	document.getElementById('discord').play();
+	setTimeout(function(){
+		window.location.href='https://www.youtube.com/results?search_query=Mangel';
+	}, 500);
+
+}
+
+function Quaquity() {
+	document.getElementById('discord').play();
+	setTimeout(function(){
+		window.location.href='https://www.youtube.com/results?search_query=Quaquity';
+	}, 500);
+
+}
+
+function what() {
+	updateAnswer();
+	const elem = new Image(50, 50); // width, height
+	elem.src = "https://ih1.redbubble.net/image.845348348.0152/flat,128x,075,f-pad,128x128,f8f8f8.u2.jpg";
+	document.getElementById("error").appendChild(elem);
+}
+var respuestas_correctas = 0;
+function yes() {
+	if(10 == respuestas_correctas +1)
+	{
+		window.alert("10 respuestas correctas! Aprobado!")
+		respuestas_correctas += 1;
+	}
+	else{
+		respuestas_correctas += 1;
+		
+	}
+	updateAnswer();
+	const elem = new Image(50, 50); // width, height
+	elem.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvSQ_-Apto4dHKo_pdGKOtdSrGiyFXqXyRwA&usqp=CAU";
+	document.getElementById("error").appendChild(elem);
+}
+
+function updateAnswer()
+{
+	document.getElementById("respuestas").innerHTML = respuestas_correctas.toString(	);
+	
+}
+
+function Thecnoblade(){
+	const elem = new Image(1000, 1000);
+	elem.src = "https://images-na.ssl-images-amazon.com/images/I/41mBYmyhskS._SX331_BO1,204,203,200_.jpg" ;
+	document.getElementById("tecno").appendChild(elem);
+}
